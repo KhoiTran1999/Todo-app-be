189f1026c63493d357c7a12bdf15426a01550b0d
Khoitran1999 <tranquockhoi1999@gmail.com>
2023-09-26 10:38:11 +0700

    fix wrong table name

--- migrations/20230926031155-Label.js
+++ migrations/20230926031155-Label.js
@@ -24,6 +24,18 @@ module.exports = {
           key: 'id',
         },
       },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal(
+          'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
+        ),
+      },
     });
   },
 
