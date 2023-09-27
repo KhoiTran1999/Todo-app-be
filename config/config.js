598cb7009866e3c441778ae7d694c7e0a67875e6
Khoitran1999 <tranquockhoi1999@gmail.com>
2023-09-27 22:26:05 +0700

    #

--- config/config.js
+++ config/config.js
@@ -9,17 +9,18 @@ module.exports = {
     dialect: env.MYSQL_DIALECT,
   },
   test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
     username: env.MYSQL_USERNAME,
     password: env.MYSQL_PASSWORD,
+    database: env.MYSQL_DATABASE,
+    host: env.MYSQL_HOST,
+    dialect: env.MYSQL_DIALECT,
+  },
+  production: {
+    production: {
+    username: 'root',
+    password: 'khoitran990120',
     database: 'mysql',
     host: 'mysql',
     dialect: 'mysql',
   },
-};
+}
